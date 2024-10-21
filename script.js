19bc9a635139adab19cad0d6063a1875d7da9264
Your Name <you@example.com>
2024-10-21 22:50:05 +0530

    added all files

---
+++ script.js
@@ -0,0 +1,29 @@
+//1
+let str="hello world !";
+let result=str.replace("world!","My dear friend");
+console.log(result);
+
+//2
+let a="Hungry"
+let repeat=(a+ " ").repeat(10);
+console.log(repeat);
+
+//3
+let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+let index=sentence.lastIndexOf("fox");
+console.log(index);
+
+//4
+let message = "Hello, world!";
+console.log(message.startsWith("hi"));
+
+//5th
+let filenme="document.pdf";
+console.log(filenme.endsWith(".pdf"));
+
+
+
+
+
+
+
